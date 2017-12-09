d149828875cf75f744906334f28539ce483b7b06
Kazumi Karbowski <kazkazinus@gmail.com>
2017-12-09 04:46:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 
-function destructivelyAppendKitten(array, name){
-  array = array.push(name)
+function destructivelyAppendKitten(array){
+  array = array.push()
   return array
 }
