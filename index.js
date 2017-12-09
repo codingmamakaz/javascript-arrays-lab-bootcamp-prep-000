33bb2ce986a1ad0682856fc7441615075eb049bc
Kazumi Karbowski <kazkazinus@gmail.com>
2017-12-09 05:19:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function appendKitten(name){
   return [...kittens, name]
 }
 
-function appendKitten(name){
-  return [...kittens, name]
+function prependKitten(name){
+  return [name, ...kittens]
 }
